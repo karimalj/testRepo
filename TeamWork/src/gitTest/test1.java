0037efb847365cd744790b3e4eced85f2705fc87
Abdulkarim Al Jamal <abdulkarim.alj@outlook.com>
2022-06-14 22:14:59 +0300

    This is a test to see whether committing a git has worked

---
+++ TeamWork/src/gitTest/test1.java
@@ -0,0 +1,21 @@
+package gitTest;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		double x = Math.random(); //coin flip
+		
+		if (x>=0.5) {
+			System.out.println("Heads");
+		}
+		else {
+			System.out.print("Tails");
+		}
+		
+		
+	}
+
+}
