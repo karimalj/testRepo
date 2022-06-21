3081c4e9238d89a0bc4f80e3370185a7238ff0bc
Abdulkarim Al Jamal <93076948+karimalj@users.noreply.github.com>
2022-06-22 02:12:52 +0300

    Update test1.java

--- TeamWork/src/gitTest/test1.java
+++ TeamWork/src/gitTest/test1.java
@@ -4,12 +4,9 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-
-		
-		// this should now be empty
-		
-
+	
+		// testing fetch
+	
 	}
 
 }
